302c2f042a56416e8434fde7aab698ae4057cab1
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2023-06-18 12:21:16 -0400

    Lint

--- src/year2/pages/Song.tsx
+++ src/year2/pages/Song.tsx
@@ -1,25 +1,6 @@
 import React from 'react'
-import Countdown, { CountdownRenderProps } from 'react-countdown'
 import { Textbox } from '../components/Textbox'
 import classesVideo from '../../year1/components/Video.module.scss'
-
-const countdownRenderer = ({
-  days,
-  hours,
-  minutes,
-  seconds,
-  completed
-}: CountdownRenderProps): JSX.Element => {
-  if (completed) {
-    return <>Soon</>
-  }
-  // Render countdown
-  return (
-    <>{days} days, {hours}h:{minutes}m:{seconds}s</>
-  )
-}
-
-const releaseDate = '2023-06-17T16:59:00+02:00'
 
 export default function Song (): JSX.Element {
   return (
